4f9590ef5fb97d28816f2aa3d593add076a269fd
Nick Jansen <nickjansen12312@gmail.com>
2023-07-10 13:49:06 -0500

    we did it

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+  const buttonTextItems = [
+    'Bears, beets, battlestar galactica',
+    "What's Forrest Gump's password? 1Forrest1",
+    'Where do programmers like to hang out? The Foo Bar'
+  ];
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const initialGameState = {
+    victory: false,
+    startTime: null,
+    endTime: null
+  };
+
+  const [userText, setUserText] = useState('');
+  const [snippet, setSnippet] = useState('');
+  const [gameState, setGameState] = useState(initialGameState);
+  
+  const updateUserText = (event) => {
+    setUserText(event.target.value);
+
+    if(event.target.value === snippet) {
+      setGameState({
+        ...gameState,
+        victory: true,
+        endTime: new Date().getTime() - gameState.startTime
+      });
+    }
+  };
+
+  const chooseSnippet = (index) => {
+    setSnippet (buttonTextItems[index]);
+    setGameState({...gameState, startTime: new Date().getTime() });
+  }
+
+  return (
+    <div>
+      <h2>TypeRace</h2>
+      <hr />
+      <h3>Snippet</h3>
+      <div>{snippet}</div>
+      <h4>{gameState.victory ? `Done! Woot! Time: ${gameState.endTime}ms` : null}</h4>
+      <input onChange={updateUserText} value = {userText}/>
+      <hr />
+      {buttonTextItems.map((textItem, index) => <button onClick={() => chooseSnippet(index)}>{textItem}</button>)}
     </div>
   );
 }
+
 
 export default App;
